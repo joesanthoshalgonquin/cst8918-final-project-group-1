da1dd50011ae677057538dce52dddc7133662417
joesanthoshalgonquin <sant0232@algonquinlive.com>
2024-04-13 17:04:29 -0400

    Added workflow to push docker image to acr

--- remix/main.tf
+++ remix/main.tf
@@ -11,6 +11,18 @@ resource "azurerm_container_registry" "acr" {
   sku                 = "Basic"
 }
 
+output "acr_server" {
+  value = azurerm_container_registry.acr.login_server
+}
+
+output "acr_username" {
+  value = azurerm_container_registry.acr.admin_username
+}
+
+output "acr_password" {
+  value = azurerm_container_registry.acr.admin_password
+}
+
 # AKS Clusters
 resource "azurerm_kubernetes_cluster" "aks_test" {
   name                = "aks_test_cluster"
